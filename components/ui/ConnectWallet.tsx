63039e5d33406582646021c3b4e1b14f50b25f2d
Mayu <mayuran.ganesathas@gmail.com>
2022-05-12 16:35:21 -0400

    Fixed compiler errors, missing modules (emotion/styled)

--- components/ui/ConnectWallet.tsx
+++ components/ui/ConnectWallet.tsx
@@ -1,6 +1,23 @@
+import { providers, ethers } from "ethers";
+import { providerOptions } from "../../lib/walletConnect";
+import Web3Modal from "web3modal";
+import { useEffect } from "react";
+
 export interface ConnectWalletProps {}
 
+// docs.walletconnect.com/quick-start/dapps/web3-provider
 export const ConnectWallet = ({}: ConnectWalletProps) => {
+  const web3Modal = new Web3Modal({
+    cacheProvider: true, // optional
+    providerOptions, // required
+  });
+  useEffect(() => {
+    const instance = await web3Modal.connect();
+  });
+
+  const provider = new ethers.providers.Web3Provider(instance);
+  const signer = provider.getSigner();
+
   return <div></div>;
 };
 
--- pages/lend.tsx
+++ pages/lend.tsx
@@ -1,10 +1,16 @@
+import { providers } from "ethers";
+import React from "react";
+import ConnectWallet from "../components/ui/ConnectWallet";
+
 export interface LendingPageProps {}
 
 export const LendingPage = ({}: LendingPageProps) => {
   return (
     <div>
       {/* Main Div ** Responsive Design Here*/}
-      <div>Wallet Connect Button {/* Wallet Connect*/}</div>
+      <div>
+        <ConnectWallet />
+      </div>
       <div>Lend</div>
       <div>DAO Table Details</div>
       <div>Extra Details and Graph Info</div>
