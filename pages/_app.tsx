3c626b2bd1886e5099ff294db2ed7d900edd62d2
Mayu <mayuran.ganesathas@gmail.com>
2022-05-13 18:09:24 -0400

    adding context, you can now access connect wallet, account connected and check if wallet is connected accross app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,14 @@
 import "../styles/globals.css";
 
 import type { AppProps } from "next/app";
+import { WalletAuthProvider } from "../lib/walletContext";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <WalletAuthProvider>
+      <Component {...pageProps} />
+    </WalletAuthProvider>
+  );
 }
 
 export default MyApp;
