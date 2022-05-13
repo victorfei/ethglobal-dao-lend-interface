78ea32da7d707f60bd7b34c381034be7bba34f7b
Mayu <mayuran.ganesathas@gmail.com>
2022-05-13 18:09:24 -0400

    adding context, you can now access connect wallet, account connected and check if wallet is connected accross app

--- components/ui/ConnectWallet.tsx
+++ components/ui/ConnectWallet.tsx
@@ -1,36 +1,19 @@
 import { providers, ethers } from "ethers";
-import { instantiateWallet } from "../../lib/walletConnect";
 import Web3Modal from "web3modal";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Button } from "@mui/material";
-import WalletConnectProvider from "@walletconnect/web3-provider";
 
-export interface ConnectWalletProps {}
+export interface ConnectWalletProps {
+  connectWallet: () => void;
+}
 
 // docs.walletconnect.com/quick-start/dapps/web3-provider
-const ConnectWalletButton = ({}: ConnectWalletProps) => {
-  //   const ConnectWallet = async () => {
-  //     const providerOptions = new WalletConnectProvider({
-  //       rpc: {
-  //         1: process.env.ALCHEMY_URL_KEY,
-  //       },
-  //     });
-
-  //     const web3Modal = new Web3Modal({
-  //       cacheProvider: true, // optional
-  //       providerOptions, // required
-  //     });
-
-  //     const instance = await web3Modal.connect();
-
-  //     const provider = new ethers.providers.Web3Provider(instance);
-  //     const signer = provider.getSigner();
-  //   };
+const ConnectWalletButton = ({ connectWallet }: ConnectWalletProps) => {
   return (
     <div>
-      <Button variant="contained" onClick={() => console.log("do nothing")}>
+      <Button variant="contained" onClick={() => connectWallet()}>
         {" "}
-        Go to Lend
+        Connect Wallet
       </Button>
       <div className="text-red-500"> hello </div>
     </div>
