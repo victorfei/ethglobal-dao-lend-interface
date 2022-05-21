3fc6b25c094a5f4f3a2b44e959755dc22326e4ac
Mayu <mayuran.ganesathas@gmail.com>
2022-05-21 17:34:07 -0400

    fadded navbar

--- pages/borrow.tsx
+++ pages/borrow.tsx
@@ -1,8 +1,9 @@
-import { useEffect } from "react";
+import React, { useEffect } from "react";
 import { BorrowForm } from "@src/components/forms/BorrowForm";
 import ConnectWallet from "@src/components/ui/ConnectWallet";
 import { useWalletAuth } from "@src/lib/walletContext";
 import { Web3Provider } from "@ethersproject/providers";
+import { NavBar } from "@src/components/navbar/NavBar";
 
 export default function Borrow() {
   const { connectWallet, checkIfWalletIsConnected, currentAccount } =
@@ -13,13 +14,12 @@ export default function Borrow() {
   }, []);
 
   return (
-    <div className="grid grid-cols-1">
-      <BorrowForm />
-
-      <ConnectWallet
-        connectWallet={connectWallet}
-        connected={currentAccount.length > 0}
-      />
+    <div className="flex flex-wrap justify-center ">
+      <NavBar />
+      <div className="shadow-xl rounded-xl py-14">
+        {" "}
+        <BorrowForm />
+      </div>
     </div>
   );
 }
