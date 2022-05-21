75b1839903a4656237fe1cc853474ea8db6a072a
Mayu <mayuran.ganesathas@gmail.com>
2022-05-21 01:36:14 -0400

    mg added section 3

---
+++ components/SectionThree.tsx
@@ -0,0 +1,21 @@
+import Button from "@mui/material/Button";
+import Router from "next/router";
+import Grid from "@mui/material/Grid";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import CardContent from "@mui/material/CardContent";
+import { Card, Typography, CardActions, Container } from "@mui/material";
+import { ArrowCircleDown } from "@mui/icons-material";
+import LendingTable from "./LendingTable";
+
+export interface SectionThreeProps {}
+
+export const SectionThree = () => {
+  return (
+    <div className=" flex-nowrap text-center justify-center   bg-gray-50 rounded-xl">
+      <div className="py-8">Our Top Performing DAO's</div>
+      <LendingTable />
+    </div>
+  );
+};
+
+export default SectionThree;
